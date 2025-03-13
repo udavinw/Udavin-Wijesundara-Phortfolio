c408a6c979286d6b3101f2798d81806339cee0f6
udavinw <65700932+udavinw@users.noreply.github.com>
2025-03-13 16:09:48 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,7 +15,7 @@ export const LINKS = [
   { href: "#work", label: "Work" },
   { href: "#about", label: "About" },
   { href: "#experience", label: "Experience" },
-  { href: "#testimonials", label: "Testimonials" },
+  // { href: "#testimonials", label: "Testimonials" },
   { href: "#contact", label: "Contact" },
 ];
 
